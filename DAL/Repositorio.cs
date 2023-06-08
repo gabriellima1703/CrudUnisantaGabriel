cc1a00559b0496b4b2b11c91d68e16b270889f77
limag <limag@LAPTOP-Q7IS79NH>
2023-06-07 22:12:39 -0300

    Adicionar arquivos de projeto.

---
+++ DAL/Repositorio.cs
@@ -0,0 +1,49 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Repositorio
+    {
+        private IMongoCollection<Agenda> _agenda;
+
+        public List<Agenda> ListaAgenda
+        {
+            get {
+                var filter = Builders<Agenda>.Filter.Empty;
+                return _agenda.Find<Agenda>(filter).ToList<Agenda>(); }
+        }
+
+        public void Create(Agenda agenda)
+        {
+            _agenda.InsertOne(agenda);
+        }
+
+        public void Delete(Agenda agenda)
+        {
+            var filter = Builders<Agenda>.Filter.Eq("Id", agenda.Id);
+            _agenda.DeleteOne(filter);
+
+        }
+
+        public void Udpate (Agenda agenda)
+        {
+            var filter = Builders<Agenda>.Filter.Eq("Id", agenda.Id);
+            var update = Builders<Agenda>.Update.Set("Nome", agenda.Nome).Set("Telefone", agenda.Telefone).Set("Endereco", agenda.Endereco);
+        }
+
+        public Repositorio()
+        {
+            var mongoClient = new MongoClient("mongodb+srv://Gabriel216261:216261@cluster0.i68cd6v.mongodb.net/");
+            var mongoDatabase = mongoClient.GetDatabase("apdesktop");
+            _agenda = mongoDatabase.GetCollection<Agenda>("agenda");
+
+        }
+    }
+   
+}
+
